4a8bfcdad596bfc97dffcb27ab38c78f675fe05e
MishariAldoub <MEAldoub@pifss.gov.kw>
2025-04-06 11:16:43 +0300

    Mishari and Abdulrahman

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from '../header/header.component';
+import { SidebarComponentComponent } from '../sidebar-component/sidebar-component.component';
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [HeaderComponent, SidebarComponentComponent],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrl: './layout.component.css',
 })
-export class LayoutComponent {
-
-}
+export class LayoutComponent {}
